39bc56aafb82cb44bef628128d592b7a00875a41
Aaron Klapatch <klapatchaaron@gmail.com>
2025-03-18 01:21:29 +0000

    Added tests and target update logic

--- mwm.tcl
+++ mwm.tcl
@@ -1,17 +1,20 @@
 #!/usr/bin/env tclsh
 
+set main_target "main"
+set cd_dir "."
+set mwm_file mwmfile.tcl
+set verbose 0
+
 proc p_usage {} {
 	puts "$::argv0 usage:
     --cd <path>
         Changes to <path> before executing commands.
     --file <other-mwmfile.tcl>
         Source targets from <other-mwmfile.tcl> instead of mwmfile.tcl
+    --target <main-target>
+        Build this target instead of \"$main_target\"
         "
 }
-
-set cd_dir "."
-set mwm_file mwmfile.tcl
-set verbose 0
 
 for {set i 0} {$i < $argc} {incr i} {
 	set arg [lindex $argv $i]
@@ -27,6 +30,13 @@ for {set i 0} {$i < $argc} {incr i} {
                 error "No folder provided for --cd"
             }
 			set cd_dir [lindex $argv $i]
+        }
+		--target {
+			incr i
+            if {$argc == $i} {
+                error "No target provided for --target"
+            }
+			set main_target [lindex $argv $i]
         }
 		--file {
 			incr i
@@ -45,12 +55,12 @@ for {set i 0} {$i < $argc} {incr i} {
 # Store values in a {up-to-date inputs outputs}
 set g_targets [dict create]
 
-proc make_target {name inputs outputs} {
+proc make_target {name outputs inputs command} {
     global g_targets
     if {[dict exists $g_targets $name]} {
         error "$name is already a target!"
     }
-    set add_val {0 $inputs $outputs}
+    set add_val {0 $outputs $inputs $command}
     dict set g_targets $name $add_val
     if {$verbose} {
         puts "Adding target $name with these values $add_val"
@@ -66,3 +76,91 @@ if {[file isfile $mwm_file] == 0} {
     error "$mwm_file does not exist or is not a file!"
 }
 source $mwm_file
+
+# This file holds the lengths for each file.
+set data_file .mwmdata.tsv
+set g_f_lens [dict create]
+if {[file exists $data_file]} {
+    # This should update g_f_lens
+    set data_f [open $data_file r]
+    while {[gets $data_f line] >= 0} {
+        # TODO: Use a regex?
+        set len_start [string last "\t" $line]
+        set len [string range $len_start+1 end]
+        set f_name [string range 0 $len_start-1]
+        dict set g_f_lens $f_name $len
+    }
+    close $data_f
+}
+
+# TODO: Optimize for multithreading.
+proc update_target {t_name} {
+    global g_targets
+    if {[dict exists $g_targets $t_name] == 0} {
+        error "Target $t_name does not exist!"
+    }
+    set t_info [dict get g_targets $t_name]
+    set up_to_date 1
+    foreach input [lindex $t_info 2] {
+        if {[file isfile $input]} {
+            if {[dict exists $g_f_lens]} {
+                set f_len [dict get $g_f_lens $input]
+                set f_size [file size $input]
+                if {[string compare $f_len $f_size] != 0} {
+                    if {$verbose} {
+                        puts "$input is out of date old len=$f_len, new len=$f_size"
+                    }
+                    set up_to_date 0
+                    break
+                }
+            }
+        } elseif {[file isdirectory $input]} {
+            if {$verbose} {
+                puts "\"$input\" is a folder, skipping"
+            }
+        } elseif {[dict exists $g_targets $input} {
+            if {$verbose} {
+                puts "Updating \"$input\" for \"$t_name\""
+            }
+            update_target $input
+        } else {
+            error "$input for target \"$t_name\" is not a file or a target!"
+        }
+    }
+    if {$up_to_date == 0} {
+        set command [lindex $t_info 3]
+        if {$verbose} {
+            puts "Running $command for $t_name"
+        }
+        # Assume the file is an output of the command.
+        if {[llength [info procs $command]] == 1} {
+            # Run this TCL proc
+            $command
+        } else {
+            exec >@stdout 2>@stderr {*}$command
+        }
+        global g_f_lens
+        foreach output [lindex $t_info 1] {
+            if {[file exists $output] == 0} {
+                error "Output \"$output\" does not exist after an update!"
+            } elseif {[file isfile $output]} {
+                set new_sz [file size $output]
+                if {$verbose} {
+                    puts "Updating size for \"$output\" to $new_sz"
+                }
+                dict set $g_f_lens $output $new_sz
+            }
+        }
+    }
+}
+
+update_target $main_target
+
+if {$verbose} {
+    puts "Updating $data_file with new lengths"
+}
+set data_f [open $data_file w]
+dict for {f_name sz} $g_f_lens {
+    puts $data_f "$f_name\t$sz"
+}
+close $data_f
--- tests/test.tcl
+++ tests/test.tcl
@@ -1,28 +1,29 @@
 #!/usr/bin/env tclsh
 
-set parent_dir [file normalize [info script]]
-set parent_dir [file dirname [file dirname $parent_dir]]
+set test_dir [file normalize [info script]]
+set test_dir [file dirname $test_dir]
+set parent_dir [file dirname $test_dir]
 cd $parent_dir
 set mwm [file join $parent_dir mwm.tcl]
 
 set test_list [dict create  \
-    "No folder provided" {--cd} \
-    "does not exist or is not a folder" {--cd bad-dir} \
-    "does not exist or is not a file" {--file bad-file} \
-    "No file provided for --file" {--file} \
+    "No folder provided" "--cd" \
+    "does not exist or is not a folder" "--cd bad-dir" \
+    "does not exist or is not a file" "--file bad-file" \
+    "No file provided for --file" "--file" \
+    "No target provided for --target" "--target" \
+    "Target bad-target does not exist" "--target bad-target --file [file join $test_dir empty-mwmfile.tcl]" \
 ]
-
 dict for {text args} $test_list {
     if {[catch {exec  $mwm {*}$args 2>@1} output] == 0} {
         error "This test should have failed! $args output=$output"
     } else {
         if {[string match "*$text*" $output] == 0} {
-            error "$args test failed! Failed to find \"$text\" in $output"
+            error "Test failed! ($args) Failed to find \"$text\" in $output"
         } else {
-            puts "$args Test succeeded."
+            puts "Test succeeded. ($args)"
         }
     }
 }
 
 puts "Yippee! All tests passed!"
-
